2f296f010e1de9c22da8246ae9b153e33a629d9c
M Vascon <m_vascon@outlook.com>
2023-12-17 16:51:58 -0300

    feat: adding current temperature feat

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,7 @@
 const { Command } = require('commander');
 const program = new Command();
 const { currentTemperature } = require('../lib/index');
+const { temperatureSelector } = require('../lib/index');
 const package = require('../package.json');
 
 program
@@ -9,10 +10,19 @@ program
   .description(package.description)
   .version(package.version);
 
-program.command('current')
-  .description('Shows the current weather')
-  .action(async () => {
-    console.log(await currentTemperature());
-  });
+// program.command('current')
+//   .description('Shows the current weather')
+//   .argument('<city>', 'name of the desired city')
+//   .action(async (city) => {
+//     console.log(await currentTemperature(city));
+//   });
+
+program.command('temperature')
+  .description('Shows temperature information according the parameters provided')
+  .argument('<city>', 'Name of the desired city')
+  .option('-c, --current', 'to obtain current information for this city')
+  .action(async (args, options) => {
+    console.log(await temperatureSelector(args, options));
+  })
 
 program.parse();
