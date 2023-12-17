3f8be09f441ccfed77a3f2d3e1ab1864c7646604
M Vascon <m_vascon@outlook.com>
2023-12-17 19:01:32 -0300

    feat; hourly temperature forecast

--- bin/index.js
+++ bin/index.js
@@ -13,7 +13,8 @@ program
 program.command('temperature')
   .description('Shows temperature information according the parameters provided')
   .argument('<city>', 'Name of the desired city. For cities which name has more than one word please use "" !')
-  .option('-c, --current', 'to obtain current information for this city')
+  .option('--hourly', 'Provides hourly temperature forecast for the informed city')
+  .option('-c, --current', 'Provides current information for this city')
   .action(async (args, options) => {
     console.log(await temperatureSelector(args, options));
   })
